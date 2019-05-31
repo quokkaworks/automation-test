efa3616b9e1c3f4c7707da12a4355591fef9e309
Glen Downton <50856733+GlenDownton@users.noreply.github.com>
2019-05-31 11:42:51 +0800

    Update packagetemplate.groovy
    getting actual steps to run #4

--- packagetemplate.groovy
+++ packagetemplate.groovy
@@ -3,6 +3,7 @@
                 	node {
         				stage('Configure') {
                				echo 'Checking out ..'
+					checkout scm
         				}
        					stage('Extract') {
 	               			bat( "echo 'Testing from within bat ..'" )
